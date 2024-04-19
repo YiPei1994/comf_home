b6240829e854c3ea3798391dcef437c7e894177a
yipei <studentypz@gmail.com>
2024-04-19 16:15:24 +0200

    style - fix style

--- src/contents/home/Welcome.tsx
+++ src/contents/home/Welcome.tsx
@@ -8,21 +8,21 @@ function Welcome() {
   const [showSlide, setShowSlide] = useState(true);
   return (
     <div
-      className={`${showSlide ? 'top-0' : '-top-full'} absolute left-0 z-40 flex  min-h-screen  w-full  flex-col  items-center overflow-hidden bg-neutral-900  transition-all duration-300`}
+      className={`${showSlide ? 'top-0' : '-top-full'} absolute left-0 z-40 flex  h-screen  w-full  flex-col items-center justify-center overflow-hidden bg-neutral-900  transition-all duration-300`}
     >
-      <div className="mt-[40px] flex h-full flex-col items-center justify-center gap-[40px] p-4 text-center xl:gap-[100px]">
+      <div className="flex h-full flex-col items-center justify-center gap-6 px-4 py-[40px] text-center xl:gap-12 xl:py-[80px]">
         <h3 className="text-primary ">Welcome</h3>
         <h2 className="bold bold text-white">
           Find your own piece of <span className="text-purple-200">Art</span>
         </h2>
-        <div className="relative my-[20px] flex h-[350px] w-[60%] xl:h-auto xl:w-full xl:gap-16">
+        <div className="relative my-[20px] flex min-h-[350px] w-[70%] md:min-h-[420px] lg:w-[55%] xl:h-auto xl:w-full xl:gap-16">
           {productsData?.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
         <Button
           onClick={() => setShowSlide((d) => !d)}
-          className="mt-[8%] bg-transparent xl:mt-0"
+          className="mt-[50px] bg-transparent lg:mt-[30px] xl:mt-0"
           type="button"
         >
           <MdArrowUpward className="text-primary animate-bounce text-4xl" />
